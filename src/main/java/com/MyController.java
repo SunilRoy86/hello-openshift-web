1a6fa4fae72b49eefcb3ab99de68f78808cf0420
Sunil Roy <sunilroy.india@gmail.com>
2022-11-22 14:31:57 +0530

    first commit

---
+++ src/main/java/com/MyController.java
@@ -0,0 +1,26 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+class MyController
+{
+	 @RequestMapping(value = "/index",method = RequestMethod.GET)
+	public String home(Model model) {
+		System.out.println("controller..");
+		model.addAttribute("message","I love India...");
+		return "home";
+	}
+	 
+	 @GetMapping("/profile")
+		public String profile(ModelAndView mv) {
+			System.out.println("controller..");
+			
+			return "profile";
+		}
+}
